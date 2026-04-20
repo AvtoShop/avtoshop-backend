4b4f4dc2e6999d890b756d0f9fb4656cc79e7c57
starsden <159563356+starsden@users.noreply.github.com>
2026-04-20 12:08:47 +0300

    upd cors

--- app/main.py
+++ app/main.py
@@ -16,8 +16,8 @@ from app.services.seeder import seed_admin, seed_sample_data
 app = FastAPI(title=settings.app_name)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origin_list,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
